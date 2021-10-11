3a6fcd045388c270cf221ea696b054c92c67848a
Ubuntu <ubuntu@ip-172-31-29-121.us-east-2.compute.internal>
2021-10-11 01:08:59 +0000

    Merge Conflict Practice

--- customers.sql
+++ customers.sql
@@ -1,5 +1,5 @@
-SELECT o.customer_id, c.name, c.email, MIN(o.created_at) AS first_order_at,
-COUNT(o.customer_id) AS number_of_orders FROM 
+SELECT o.customer_id, c.name, c.email, MIN(o.created_at) AS first_orders_at,
+COUNT(o.customer_id) AS numbers_of_orders FROM
 `analytics-engineers-club.coffee_shop.orders` o RIGHT JOIN 
 `analytics-engineers-club.coffee_shop.customers` c ON o.customer_id = c.id
 GROUP BY o.customer_id, c.name, c.email
