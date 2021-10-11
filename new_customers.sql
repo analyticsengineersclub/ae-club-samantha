d4078bbe1c600831dcc8db10bdff3613f7fdb2ba
Ubuntu <ubuntu@ip-172-31-29-121.us-east-2.compute.internal>
2021-10-11 00:19:58 +0000

    New customers query

--- new_customers.sql
+++ new_customers.sql
@@ -2,4 +2,5 @@ SELECT orders.customer_id, customers.name, customers.email, MIN(orders.created_a
 COUNT(orders.customer_id) AS number_of_orders FROM 
 `analytics-engineers-club.coffee_shop.orders` orders  RIGHT JOIN 
 `analytics-engineers-club.coffee_shop.customers` customers  ON orders.customer_id = customers.id
-GROUP BY orders.customer_id, customers.name, customers.email;
+GROUP BY orders.customer_id, customers.name, customers.email
+order by first_order_at limit 5;
